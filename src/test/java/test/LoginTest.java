a351d7388ce4bab6767934c99b67ebd898f65b15
theva <thevakajan3@gmail.com>
2020-05-11 15:08:00 +0530

    nsu

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -10,15 +10,15 @@ import utils.TestBase;
 
 public class LoginTest extends TestBase {
 
-//    @Test
-//    public void LoginTest(){
-//        softAssert = new SoftAssert();
-//        softAssert.assertTrue(LoginPage.isHrmLoginPageDisplayed(),"HrmLogin Page Not Display") ;
-//        LoginPage.setUserName("Admin");
-//        LoginPage.setPass("admin123");
-//        LoginPage.ClickLogin();
-//        softAssert.assertTrue(DashboardPage.isHrmDashboardPageDisplayed(),"Hrm Dashboard Page Not Display") ;
-//        softAssert.assertAll();
-//
-//    }
+    @Test
+    public void LoginTest(){
+        softAssert = new SoftAssert();
+        softAssert.assertTrue(LoginPage.isHrmLoginPageDisplayed(),"HrmLogin Page Not Display") ;
+        LoginPage.setUserName("Admin");
+        LoginPage.setPass("admin123");
+        LoginPage.ClickLogin();
+        softAssert.assertTrue(DashboardPage.isHrmDashboardPageDisplayed(),"Hrm Dashboard Page Not Display") ;
+        softAssert.assertAll();
+
+    }
 }
