9848776c3830bb8990349dbf6374ad133a77c6ec
theva <thevakajan3@gmail.com>
2020-05-11 13:47:04 +0530

    hrm

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.pages.DashboardPage;
+import com.pages.LoginPage;
+import org.testng.annotations.Test;
+
+import org.testng.asserts.SoftAssert;
+import utils.PageBase;
+import utils.TestBase;
+
+public class LoginTest extends TestBase {
+
+    @Test
+    public void LoginTest(){
+        softAssert = new SoftAssert();
+        softAssert.assertTrue(LoginPage.isHrmLoginPageDisplayed(),"HrmLogin Page Not Display") ;
+        LoginPage.setUserName("Admin");
+        LoginPage.setPass("admin123");
+        LoginPage.ClickLogin();
+        softAssert.assertTrue(DashboardPage.isHrmDashboardPageDisplayed(),"Hrm Dashboard Page Not Display") ;
+        softAssert.assertAll();
+
+    }
+}
---
+++ src/test/java/utils/TestBase.java
@@ -0,0 +1,57 @@
+package utils;
+
+import org.apache.log4j.Logger;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.asserts.SoftAssert;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+
+public class TestBase {
+    public SoftAssert softAssert;
+    
+
+    private static final Logger LOGGER = Logger.getLogger(TestBase.class);
+
+    @BeforeTest(alwaysRun = true)
+    public void beforeTest() {
+        System.out.println("Test Running " + this.getClass().toString());
+    }
+//ajxhusduhdusssssssssssssssss
+    @BeforeMethod(alwaysRun = true)
+    public void loadBrowser() {
+        LOGGER.info("Initiate Browser");
+        try {
+            PageBase.initiateDriver();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        LOGGER.info("Browser Initiated");
+    }
+    //ajxhusduhdusssssssssssssssss
+    @BeforeMethod(alwaysRun = true)
+    public void beforeMethod() {
+        softAssert = new SoftAssert();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void nameBefore(Method method) {
+        LOGGER.info("Test name: " + method.getName());
+    }
+
+    //ajxhusduhdusssssssssssssssss
+    @AfterMethod(alwaysRun = true)
+    public void closeBrowser() {
+        LOGGER.info("Closing Browser");
+        PageBase.closeDriver();
+        LOGGER.info("Browser Closed");
+    }
+    //ajxhusduhdusssssssssssssssss
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod(Method method, ITestResult result) {
+        LOGGER.info("Executed test case name:" + method.getName() + " Execution Results : " + result.toString());
+    }
+}
