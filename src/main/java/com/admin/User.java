164f804c1371a178073e30123fb6d812e0923d8f
Vinthusha <vinthushalogan@gmail.com>
2020-05-14 11:36:02 +0530

    vunthusha AdminPage

--- src/main/java/com/admin/User.java
+++ src/main/java/com/admin/User.java
@@ -10,79 +10,75 @@ import java.util.logging.Logger;
 
 public class User  extends PageBase {
     private static final Logger LOGGER = Logger.getLogger(String.valueOf(User.class));
+    //------------------------------------------------
     private static By Admin = By.id("menu_admin_viewAdminModule");
     private static By UserManagement= By.id("menu_admin_UserManagement");
     private static By User = By.id("menu_admin_viewSystemUsers");
-  //  private static By SystemUserHeader = By.id("//*[@id=\"systemUser-information\"]/a");
-    private static By TxtUsername= By.id("searchSystemUser_userName");
-    private static By UserRole= By.id("searchSystemUser_userType");
+    //  private static By SystemUserHeader = By.id("//*[@id=\"systemUser-information\"]/a");
 
-    //WebElement UserRoleElement= driver.findElement(By.id("searchSystemUser_userType"));
-   // Select roleElement = new Select(UserRoleElement);
-	  //      roleElement.selectByIndex(1);
-
+    // ----------------------- User page -------------------------------
+    private static By TxtUsername    = By.id("searchSystemUser_userName");
+    private static By UserRole       = By.id("searchSystemUser_userType");
     private static By TxtEmployeeName= By.id("searchSystemUser_employeeName_empName");
-    private static By Status =By.id("searchSystemUser_status");
+    private static By Status         = By.id("searchSystemUser_status");
 
     private static By btnSearch =By.id("searchBtn");
     private static By btnReset =By.id("resetBtn");
+
     private static By btnAdd =By.id("btnAdd");
+    //private static By btnDelete = By.id("btnDelete");
 
-    private static By TxtUserrole =By.id("systemUser_userType");
-    private static By TxtEmpName =By.id("systemUser_employeeName_empName");
-    private static By  TxtUname=By.id("systemUser_userName");
-    private static By  clickstatus=By.id("systemUser_status");
-    private static By TxtPassword=By.id("systemUser_password");
-    private static By clickSave=By.id("btnSave");
-
-
-
+//-------------------------- Add user -----------------------------
+    private static By TxtUserrole = By.id("systemUser_userType");
+    private static By TxtEmpName  = By.id("systemUser_employeeName_empName");
+    private static By TxtUname    = By.id("systemUser_userName");
+    private static By clickstatus = By.id("systemUser_status");
+    private static By TxtPassword = By.id("systemUser_password");
+    private static By TxtConPassword = By.id("systemUser_confirmPassword");
+    private static By btnclickSave   = By.id("btnSave");
 
 
 
     public static void clickAdmin() {
         getDriver().findElement(Admin).click();
-
     }
     public static void clickUserManagement() {
         getDriver().findElement(UserManagement).click();
-
     }
     public static void clickUser() {
         getDriver().findElement(User).click();
-
     }
+
 
     public static void setTxtUsername(String username) {
         getDriver().findElement(TxtUsername).sendKeys(username);
-
     }
     public static void setUserRole(String userRole) {
-        getDriver().findElement(UserRole).sendKeys(userRole);
-
+        getDriver().findElement(UserRole).isSelected();
     }
     public static void setTxtEmployeeName(String name) {
         getDriver().findElement(TxtEmployeeName).sendKeys(name);
-
     }
     public static void setStatus() {
         getDriver().findElement(Status).click();
-
     }
+
+
     public static void clickSearch() {
         getDriver().findElement(btnSearch).click();
     }
     public static void clickReset() {
         getDriver().findElement(btnReset).click();
     }
+
     public static void clickAdd() {
         getDriver().findElement(btnAdd).click();
     }
     public static void wait(int seconds){
         PageBase.implicitWait(seconds);
     }
-    public static void clickUserRole() {
-        getDriver().findElement(TxtUserrole).click();
+    public static void clickUserRole(String UserRole) {
+        getDriver().findElement(TxtUserrole).isSelected();
     }
     public static void setEmpName(String empName) {
         getDriver().findElement(TxtEmpName).sendKeys( empName);
@@ -96,9 +92,16 @@ public class User  extends PageBase {
     public static void setTxtPassword(String password) {
         getDriver().findElement(TxtPassword).sendKeys(password);
     }
-    public static void clickSave() {
-        getDriver().findElement(clickSave).click();
+    public static void  setTxtConPassword(String conpassword) {
+        getDriver().findElement(TxtConPassword).sendKeys(conpassword);
     }
 
+    public static void clickSave() {
+        getDriver().findElement(btnclickSave).click();
+    }
+
+   /* public static void clickDelete(){
+        getDriver().findElement(btnDelete).click();
+    }*/
 }
 
--- src/test/java/test/AdminTest/UserTest.java
+++ src/test/java/test/AdminTest/UserTest.java
@@ -17,6 +17,7 @@ public class UserTest extends TestBase {
     public void UserTest(){
         PageLogin.login();
 
+// login page
         User.wait(20);
         User.clickAdmin();
         User.wait(5);
@@ -24,10 +25,11 @@ public class UserTest extends TestBase {
         User.wait(5);
         User.clickUser();
 
+// admin page
         User.wait(20);
         User.setTxtUsername("vinthusha");
         User.wait(5);
-        User.setUserRole("");
+        User.setUserRole("1");
         User.wait(5);
         User.setTxtEmployeeName("Mathu");
         User.wait(5);
@@ -39,16 +41,22 @@ public class UserTest extends TestBase {
         User.wait(5);
         User.clickAdd();
 
-        User.clickUserRole();
+// User page add
+        User.clickUserRole("0");
         User.wait(5);
-        User.setEmpName("Vinthu");
+        User.setEmpName("Steven Edwards");
         User.wait(5);
         User.clickStatus();
         User.wait(5);
-        User.setUname("Logetha");
+        User.setUname("vinthusha");
         User.wait(5);
-        User.setTxtPassword("jhvdjgvdh");
+        User.setTxtPassword("jhvdjgvdh@");
+        User.wait(5);
+        User.setTxtConPassword("jhvdjgvdh@");
         User.wait(5);
         User.clickSave();
+
+  // User page delete
+     //   User.clickDelete();
     }
 }
